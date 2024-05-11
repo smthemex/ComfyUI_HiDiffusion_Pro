d314e98b831980dbbbce2686f6f6afd98d6d38a7
smthemex <138738845+smthemex@users.noreply.github.com>
2024-05-11 10:22:59 +0800

    Add files via upload
    init

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import sys
+
+python = sys.executable
+
+from .Hidiffusion_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
